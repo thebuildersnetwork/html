7f8286aa5f9e4fcad778d4039c67aaa44b16e5ad
Tung Mang Vien <tungmv7@gmail.com>
2017-10-27 11:31:14 +0700

    init source

---
+++ js/start.js
@@ -0,0 +1,232 @@
+/**
+ * Website start here
+ */
+$(window).load(function() {
+    wow = new WOW({
+        animateClass: 'animated',
+        offset: 100,
+        callback: function(box) {
+            console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+    });
+    wow.init();
+});
+$(document).ready(function($) {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 46)
+        });
+        event.preventDefault();
+    });
+    if ($(".btn-top").length > 0) {
+        $(window).scroll(function() {
+            var e = $(window).scrollTop();
+            if (e > 100) {
+                $(".btn-top").show()
+            } else {
+                $(".btn-top").hide()
+            }
+        });
+        $(".btn-top").click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            })
+        });
+    }
+
+    if ($(".btn-searchsg").length > 0) {
+        $(".btn-searchsg").click(function() {
+            if ($(".frm-search-1").hasClass("active")) {
+                $(".frm-search-1").removeClass("active");
+            } else {
+                $(".frm-search-1").addClass("active");
+                return false;
+            }
+        });
+    }
+    $('.slick-slider-1').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false
+    });
+
+    $('.main-slider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+    if ($('.mCustomScrollbar').length > 0) {
+        $('.mCustomScrollbar').mCustomScrollbar({
+            scrollInertia: 500
+        });
+    }
+    setTimeout(function() {
+        $('.loading').fadeOut(300, function() {
+            $(this).remove();
+        });
+    }, 800);
+    $('.slider-1').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.slick-video').slick({   
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.slick-ins').slick({   
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
+$(document).click(function() {
+    $(".a").removeClass("active");
+    $(".a").removeClass("show");
+    $("#a").removeClass("normal");
+});
+$('.a').click(function(event) {
+    event.stopPropagation();
+});
+
+(function($) {
+    $.fn.menumaker = function(options) {
+        var cssmenu = $(this),
+            settings = $.extend({
+                format: "dropdown",
+                sticky: false
+            }, options);
+        return this.each(function() {
+            $(this).find(".button").on('click', function() {
+                $(this).toggleClass('menu-opened');
+                var mainmenu = $(this).next('ul');
+                if (mainmenu.hasClass('open')) {
+                    mainmenu.slideToggle().removeClass('open');
+                } else {
+                    mainmenu.slideToggle().addClass('open');
+                    if (settings.format === "dropdown") {
+                        mainmenu.find('ul').show();
+                    }
+                }
+            });
+            cssmenu.find('li ul').parent().addClass('has-sub');
+            multiTg = function() {
+                cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+                cssmenu.find('.submenu-button').on('click', function() {
+                    $(this).toggleClass('submenu-opened');
+                    if ($(this).siblings('ul').hasClass('open')) {
+                        $(this).siblings('ul').removeClass('open').slideToggle();
+                    } else {
+                        $(this).siblings('ul').addClass('open').slideToggle();
+                    }
+                });
+            };
+            if (settings.format === 'multitoggle') multiTg();
+            else cssmenu.addClass('dropdown');
+            if (settings.sticky === true) cssmenu.css('position', 'fixed');
+            resizeFix = function() {
+                var mediasize = 1000;
+                if ($(window).width() > mediasize) {
+                    cssmenu.find('ul').show();
+                }
+                if ($(window).width() <= mediasize) {
+                    cssmenu.find('ul').hide().removeClass('open');
+                }
+            };
+            resizeFix();
+            return $(window).on('resize', resizeFix);
+        });
+    };
+})(jQuery);
